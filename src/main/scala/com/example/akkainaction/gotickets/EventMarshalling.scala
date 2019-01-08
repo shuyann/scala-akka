18a80f399c66d4cb6b23c7b2be05182690180a10
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-01-09 00:44:05 +0900

    implement gotikcets rest api

--- src/main/scala/com/example/akkainaction/gotickets/EventMarshalling.scala
+++ src/main/scala/com/example/akkainaction/gotickets/EventMarshalling.scala
@@ -1,5 +1,26 @@
 package com.example.akkainaction.gotickets
 
-trait EventMarshalling {
+import spray.json._
 
+case class EventDescription(tickets: Int) {
+  require(tickets > 0)
+}
+
+case class TicketRequest(tickets: Int) {
+  require(tickets > 0)
+}
+
+case class Error(message: String)
+
+trait EventMarshalling extends DefaultJsonProtocol {
+
+  import BoxOffice._
+
+  implicit val eventDescriptionFormat = jsonFormat1(EventDescription)
+  implicit val eventFormat = jsonFormat2(Event)
+  implicit val eventFormats = jsonFormat1(Events)
+  implicit val ticketRequestFormat = jsonFormat1(TicketRequest)
+  implicit val ticketFormat = jsonFormat1(TicketSeller.Ticket)
+  implicit val ticketsFormat = jsonFormat2(TicketSeller.Tickets)
+  implicit val errorFormat = jsonFormat1(Error)
 }
--- src/main/scala/com/example/akkainaction/gotickets/Main.scala
+++ src/main/scala/com/example/akkainaction/gotickets/Main.scala
@@ -1,5 +1,58 @@
 package com.example.akkainaction.gotickets
 
-object Main {
+import scala.concurrent.Future
 
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.util.Timeout
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.stream.ActorMaterializer
+
+import com.typesafe.config.{Config, ConfigFactory}
+import scala.util.{Failure, Success}
+
+object Main extends App
+  with RequestTimeout {
+
+  val config = ConfigFactory.load()
+  // get host and port from config file
+  val host = config.getString("http.host")
+  val port = config.getInt("http.port")
+
+  implicit val system = ActorSystem()
+  // bindAndHandle need implicit ExecutionContext parameter
+  implicit val ec = system.dispatcher
+
+  val api = new RestApi(system, requestTimeout(config)).routes // The RestApi provides a Route
+
+  implicit val materializer = ActorMaterializer()
+  // start http server from Akka HTTP
+  val bindingFuture: Future[ServerBinding] =
+    Http().bindAndHandle(api, host, port)
+
+  val log = Logging(system.eventStream, "go-tickets")
+  bindingFuture.map { serverBinding =>
+    log.info(s"RestApi bound to ${serverBinding.localAddress} ")
+  }.onComplete {
+    case Success(_) =>
+      log.info("Success to bind to {}:{}", host, port)
+    case Failure(ex) =>
+      log.error(ex, "Failed to bind to {}:{}!", host, port)
+      system.terminate()
+  }
+
+
+}
+
+trait RequestTimeout {
+
+  import scala.concurrent.duration._
+
+  def requestTimeout(config: Config): Timeout = {
+    val t = config.getString("akka.http.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.length, d.unit)
+  }
 }
