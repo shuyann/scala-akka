d339e920709627888d00cb59ad3b6b6b2727154f
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-01-17 20:33:52 +0900

    add blog part one

---
+++ src/main/scala/com/example/blog/HelloActor.scala
@@ -0,0 +1,12 @@
+package com.example.blog
+
+import akka.actor.Actor
+
+class HelloActor extends Actor {
+
+  def receive = {
+    case "Hello" => println("World")
+    case "How are you?" => sender ! "I'm fine thank you!"
+    case _ => println("unknown message.")
+  }
+}
--- src/main/scala/com/example/blog/Main.scala
+++ src/main/scala/com/example/blog/Main.scala
@@ -1,5 +1,27 @@
 package com.example.blog
 
-object Main {
+import akka.actor.{ActorSystem, Props}
+import akka.util.Timeout
+import akka.pattern.ask
 
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Failure}
+
+object Main extends App {
+  implicit val timeout: Timeout = Timeout(5 seconds)
+
+  val system = ActorSystem("system")
+  val actor = system.actorOf(Props[HelloActor], "helloActor")
+
+  actor ! "Hello"
+  actor ! "foo"
+
+  val reply = actor ? "How are you?"
+  reply.onComplete {
+    case Success(res) => println(res)
+    case Failure(ex) => sys.error(ex.toString)
+  }
+
+  system.terminate()
 }
