cf99e92532935224dc6f15041e4d2c27e875db0b
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-01-17 20:37:03 +0900

    add hello actor spec case

---
+++ src/test/scala/com/example/blog/HelloActorSpec.scala
@@ -0,0 +1,7 @@
+package com.example.blog
+
+class HelloActorSpec {
+  // TODO: println("World") when receive "Hello"
+  // TODO:  "I'm fine thank you!" when receive "How are you?"
+  // TODO: println("unknown message.") when receive "foo"
+}
