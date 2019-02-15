0eaae4ccffd17bdc6b1296739051d0eb2a716a7d
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-02-15 21:16:25 +0900

    update AkkaQuickStartSpec

--- src/test/scala/com/example/official/quickstart/AkkaQuickstartSpec.scala
+++ src/test/scala/com/example/official/quickstart/AkkaQuickstartSpec.scala
@@ -1,5 +1,48 @@
 package com.example.official.quickstart
 
-class AkkaQuickstartSpec {
+import org.specs2.mutable._
+import Greeter._
+import Printer._
+
+import scala.concurrent.duration._
+import akka.actor._
+import akka.testkit._
+import org.specs2.specification.AfterAll
+
+class AkkaQuickstartSpec extends TestKit(ActorSystem("testAkkaQuickStart"))
+  with SpecificationLike
+  with DefaultTimeout
+  with ImplicitSender
+  with Tables with AfterAll {
+
+  override def afterAll = system.terminate()
+
+
+  object Fixtures {
+    val helloGreetingMessage = "hello"
+    val fooGreetingMessage = "foo"
+    val greetPerson = "Akka"
+  }
+
+  "AkkaQuickStart" >> {
+    import Fixtures._
+    "pass on a greeting message when instructed to helloGreeter" in {
+      val testProbe = TestProbe()
+      val helloGreeter = system.actorOf(Greeter.props(helloGreetingMessage, testProbe.ref))
+      helloGreeter ! WhoToGreet(greetPerson)
+      helloGreeter ! Greet
+      testProbe.expectMsg(500 millis, Greeting(helloGreetingMessage + ", " + greetPerson))
+      ok
+    }
+
+    "pass on a greeting message when instructed to fooGreeter" in {
+      val testProbe = TestProbe()
+      val fooGreeter = system.actorOf(Greeter.props(fooGreetingMessage, testProbe.ref))
+      fooGreeter ! WhoToGreet(greetPerson)
+      fooGreeter ! Greet
+      testProbe.expectMsg(500 millis, Greeting(fooGreetingMessage + ", " + greetPerson))
+      ok
+    }
+  }
 
 }
