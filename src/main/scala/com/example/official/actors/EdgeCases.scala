3e2bf461552ac49636d9aec2e343f0e25da08d0e
Shuya Yamamoto <yamamotoshu1127@gmail.com>
2019-02-18 15:29:43 +0900

    update edge cases

--- src/main/scala/com/example/official/actors/EdgeCases.scala
+++ src/main/scala/com/example/official/actors/EdgeCases.scala
@@ -18,6 +18,10 @@ class DefaultValueActor1(a: Int, b: Int = 5) extends Actor {
   }
 }
 
+object DefaultValueActor1 {
+  def props(a: Int, b: Int = 5) = Props(new DefaultValueActor1(a, b))
+}
+
 class DefaultValueActor2(b: Int = 5) extends Actor {
   def receive = {
     case x: Int => sender ! (x * b)
@@ -25,6 +29,22 @@ class DefaultValueActor2(b: Int = 5) extends Actor {
   }
 }
 
-class EdgeCases extends App {
+object DefaultValueActor2 {
+  def props(b: Int = 5) = Props(new DefaultValueActor2(b))
+}
 
+class EdgeCases extends App {
+  val system = ActorSystem("edgecases")
+
+  val actor1 = system.actorOf(DefaultValueActor1.props(10))
+  val actor2 = system.actorOf(DefaultValueActor1.props(3, 3))
+  val actor3 = system.actorOf(DefaultValueActor2.props())
+  val actor4 = system.actorOf(DefaultValueActor2.props(10))
+
+  actor1 ! 5
+  actor2 ! 5
+  actor3 ! 5
+  actor4 ! 5
+
+  system.terminate()
 }
